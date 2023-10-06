a18b698522df7f51d6aefa8ae5bf56e72c62fac7
Arijit Roy <120275227+aroyy007@users.noreply.github.com>
2023-10-07 01:56:43 +0600

    Mathematical_method

---
+++ Mathematical_method.java
@@ -0,0 +1,15 @@
+public class Mathematical_method {
+    public static void main(String[] args) {
+
+        System.out.println("Absolute value of -5: " + Math.abs(-5));
+        System.out.println("Square root of 25: " + Math.sqrt(25));
+        System.out.println("2 raised to the power of 3: " + Math.pow(2, 3));
+        System.out.println("The value of Pi: " + Math.PI);
+        System.out.println("Maximum between 2 and 3: " + Math.max(2, 3));
+        System.out.println("Minimum between 2 and 3: " + Math.min(2, 3));
+        System.out.println("Ceiling value of -5.67: " + Math.ceil(-5.67));
+        System.out.println("Rounded value of -5.34: " + Math.round(-5.34));
+        System.out.println("Floor value of -5.67: " + Math.floor(-5.67));
+        System.out.println("Random number between 0 and 1: " + Math.random());
+    }
+}
