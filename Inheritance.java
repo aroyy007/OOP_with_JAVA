f5b9b3150f69d57b39704d8d79c3ee44781c60cd
Arijit Roy <120275227+aroyy007@users.noreply.github.com>
2023-10-15 02:52:06 +0600

    Inheritance

--- Inheritance.java
+++ Inheritance.java
@@ -1,1 +1,32 @@
+class One 
+{
+    public void Print_one() {
+        System.out.println("This is class One");
+    }
+}
+
+class Two extends One //Single Inheritance
+{
+    public void Print_two() {
+        System.out.println("This is class Two");
+    }
+}
+
+class Three extends Two //Multilevel Inheritance
+{
+    public void Print_three() {
+        System.out.println("This is class Three");
+    }
+}
+
+public class Inheritance {
+    public static void main(String[] args) {
+
+        Three obj3 = new Three();
+        obj3.Print_one();
+        obj3.Print_two();
+        obj3.Print_three();
+
+    }
+}
 
